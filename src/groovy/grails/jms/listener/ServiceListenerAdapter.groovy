b30e46c318dc0c22fb4a9d553c3f11900e35aa2c
Luke Daley <ld@ldaley.com>
2009-06-15 10:04:40 +0000

    Listener adapter that is closure aware.

--- src/groovy/grails/jms/listener/ServiceListenerAdapter.groovy
+++ src/groovy/grails/jms/listener/ServiceListenerAdapter.groovy
@@ -4,5 +4,18 @@ import org.springframework.jms.listener.adapter.MessageListenerAdapter
 import javax.jms.JMSException
 
 class ServiceListenerAdapter extends MessageListenerAdapter {
-    def listenerIsClosure
+	def listenerIsClosure
+
+	protected Object invokeListenerMethod(String methodName, Object[] arguments) throws JMSException {
+		if (listenerIsClosure) {
+			try {
+				this.delegate."$methodName".call(arguments)
+			} catch(Exception e) {
+				// swallow
+				null
+			}
+		} else {
+			super.invokeListenerMethod(methodName, arguments)
+		}
+	}
 }
