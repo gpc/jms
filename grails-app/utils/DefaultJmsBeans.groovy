92248c77978401da661400ae4f0f6ed1f4b8d596
Luke Daley <ld@ldaley.com>
2009-10-04 15:06:22 +1000

    These definitions don't need to be under the “jms” key.

--- grails-app/utils/DefaultJmsBeans.groovy
+++ grails-app/utils/DefaultJmsBeans.groovy
@@ -3,28 +3,23 @@ import org.springframework.jms.support.converter.SimpleMessageConverter
 import org.springframework.jms.listener.DefaultMessageListenerContainer
 import grails.jms.listener.ServiceListenerAdapter
 
-jms {
-    templates {
-        standard {
-            clazz = JmsTemplate
-            connectionFactoryBean = "jmsConnectionFactory"
-            messageConverter = new SimpleMessageConverter()
-        }
+templates {
+    standard {
+        connectionFactoryBean = "jmsConnectionFactory"
+        messageConverter = new SimpleMessageConverter()
     }
-    containers {
-        standard {
-            clazz = DefaultMessageListenerContainer
-            concurrentConsumers = 1
-            subscriptionDurable = false
-            autoStartup = false
-            connectionFactoryBean = "jmsConnectionFactory"
-            messageSelector = null
-        }
+}
+containers {
+    standard {
+        concurrentConsumers = 1
+        subscriptionDurable = false
+        autoStartup = false
+        connectionFactoryBean = "jmsConnectionFactory"
+        messageSelector = null
     }
-    adapters {
-        standard {
-            clazz = ServiceListenerAdapter
-            messageConverter = new SimpleMessageConverter()
-        }
+}
+adapters {
+    standard {
+        messageConverter = new SimpleMessageConverter()
     }
 }
