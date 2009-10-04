c6615dcddf48be20cc52aa084935420a68945eaf
Luke Daley <ld@ldaley.com>
2009-10-04 11:29:05 +1000

    Supporting test for previous commit.

---
+++ test/unit/grails/jms/bean/JmsTemplateBeanDefinitionTests.groovy
@@ -0,0 +1,23 @@
+package grails.jms.bean
+import grails.spring.BeanBuilder
+
+class JmsTemplateBeanDefinitionTests extends GroovyTestCase {
+    
+    def bb
+    
+    void setUp() {
+        bb = new BeanBuilder()
+    }
+    
+    void testCreate() {
+        def nameBase = "example"
+        
+        def jtbd = new JmsTemplateBeanDefinition(nameBase, [meta: ['abstract': true]])
+        jtbd.register(bb)
+        def ajtbd = bb.getBeanDefinition(nameBase + JmsTemplateBeanDefinition.NAME_SUFFIX)
+        
+        assertNotNull(ajtbd)
+        assertEquals(JmsTemplateBeanDefinition.DEFAULT_CLASS.name, ajtbd.beanClassName)
+    }
+    
+}
