fa08530bb45b038a023d666b9619527ebf06b848
Luke Daley <ld@ldaley.com>
2009-10-04 13:11:55 +1000

    Builder for listener adapters.

---
+++ src/groovy/grails/jms/bean/JmsListenerAdapterAbstractBeanDefinitionBuilder.groovy
@@ -0,0 +1,17 @@
+package grails.jms.bean
+import org.springframework.jms.listener.adapter.MessageListenerAdapter
+
+class JmsListenerAdapterAbstractBeanDefinitionBuilder extends JmsBeanDefinitionBuilder {
+
+    final static nameSuffix = "JmsListenerAdapter"
+    final static defaultClazz = MessageListenerAdapter
+    
+    JmsListenerAdapterAbstractBeanDefinitionBuilder(name, definition) {
+        super(name, definition)
+    }
+    
+    def getMeta() {
+        (super.getMeta() ?: [:]) + ['abstract': true]
+    }
+
+}
---
+++ test/unit/grails/jms/bean/JmsListenerAdapterAbstractBeanDefinitionBuilderTests.groovy
@@ -0,0 +1,18 @@
+package grails.jms.bean
+import grails.spring.BeanBuilder
+
+class JmsListenerAdapterAbstractBeanDefinitionBuilderTests extends GroovyTestCase {
+    
+    def bb
+    
+    void setUp() {
+        bb = new BeanBuilder()
+    }
+    
+    void testCreate() {
+        def bdb = new JmsListenerAdapterAbstractBeanDefinitionBuilder('example', [:])
+        bdb.build(bb)
+        assertTrue(bb.getBeanDefinition(bdb.name).'abstract')
+    }
+    
+}
