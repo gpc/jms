0d8ac4f32b2d8fb1af4ca70afd007e890f763e32
Luke Daley <ld@ldaley.com>
2009-10-04 12:45:47 +1000

    Tests for previous commit.

---
+++ test/unit/grails/jms/bean/JmsBeanDefinitionBuilderTests.groovy
@@ -0,0 +1,40 @@
+package grails.jms.bean
+import grails.spring.BeanBuilder
+
+class JmsBeanDefinitionBuilderTests extends GroovyTestCase {
+    
+    def bb
+    
+    void setUp() {
+        bb = new BeanBuilder()
+    }
+    
+    void testCreate() {
+        def nameBase = "example"
+        
+        def bdb = new JmsBeanDefinitionBuilderTestsTestImpl(nameBase, [a: "a"])
+        
+        assertEquals(nameBase + JmsBeanDefinitionBuilderTestsTestImpl.nameSuffix, bdb.name)
+        assertEquals(JmsBeanDefinitionBuilderTestsTestImpl.defaultClazz, bdb.clazz)
+    }
+    
+    void testExplicitClass() {
+        def nameBase = "example"
+        
+        def bdb = new JmsBeanDefinitionBuilderTestsTestImpl(nameBase, [a: "a", clazz: String])
+        
+        assertEquals(String, bdb.clazz)
+    }
+    
+}
+
+class JmsBeanDefinitionBuilderTestsTestImpl extends JmsBeanDefinitionBuilder {
+
+    JmsBeanDefinitionBuilderTestsTestImpl(name, definition) {
+        super(name, definition)
+    }
+    
+    final static nameSuffix = "Suffix"
+    final static defaultClazz = [:].getClass()
+
+}
