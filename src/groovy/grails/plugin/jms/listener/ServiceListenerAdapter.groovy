0b6b428d016989dd8ba1c6641208ae9bed087fb7
Luke Daley <ld@ldaley.com>
2010-03-06 15:59:53 +1000

    whitespace.

--- src/groovy/grails/plugin/jms/listener/ServiceListenerAdapter.groovy
+++ src/groovy/grails/plugin/jms/listener/ServiceListenerAdapter.groovy
@@ -5,26 +5,26 @@ import javax.jms.JMSException
 
 class ServiceListenerAdapter extends MessageListenerAdapter {
 
-	def listenerIsClosure
-	def persistenceInterceptor
-	
-	protected Object invokeListenerMethod(String methodName, Object[] arguments) throws JMSException {
-		try {
+    def listenerIsClosure
+    def persistenceInterceptor
+    
+    protected Object invokeListenerMethod(String methodName, Object[] arguments) throws JMSException {
+        try {
           persistenceInterceptor.init()
-			
-			if (listenerIsClosure) {
-				try {
-					this.delegate."$methodName".call(arguments)
-				} catch(Exception e) {
-					// swallow
-					null
-				}
-			} else {
-				super.invokeListenerMethod(methodName, arguments)
-			}
-			
-		} finally {
+            
+            if (listenerIsClosure) {
+                try {
+                    this.delegate."$methodName".call(arguments)
+                } catch(Exception e) {
+                    // swallow
+                    null
+                }
+            } else {
+                super.invokeListenerMethod(methodName, arguments)
+            }
+            
+        } finally {
           persistenceInterceptor.destroy()
-		}
-	}
+        }
+    }
 }
