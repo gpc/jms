650b4b019bcb95540efa342e23cdcea8e4adf981
Luke Daley <ld@ldaley.com>
2010-04-20 09:21:46 +1000

    Version bump.

--- JmsGrailsPlugin.groovy
+++ JmsGrailsPlugin.groovy
@@ -14,7 +14,7 @@ class JmsGrailsPlugin {
     
     static LOG = LogFactory.getLog('grails.plugin.jms.JmsGrailsPlugin')
     
-    def version = "0.5"
+    def version = "0.5.1"
     def author = "Luke Daley"
     def authorEmail = "ld@ldaley.com"
     def title = "This plugin adds MDB functionality to services."
