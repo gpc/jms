a64b3f41efa149e6e7f55635618c7b7a6822346d
Luke Daley <ld@ldaley.com>
2010-05-30 18:23:55 +1000

    Fixes for pulling in bad dependencies.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,10 +3,16 @@ grails.project.dependency.resolution = {
     log "warn"
     repositories {
         grailsHome()
+        grailsCentral()
         mavenCentral()
     }  
     dependencies {
         compile 'org.apache.geronimo.specs:geronimo-jms_1.1_spec:1.1.1'
-        test 'org.apache.activemq:activemq-core:5.3.0'
+        test ('org.apache.activemq:activemq-core:5.3.0') { 
+            excludes 'activemq-openwire-generator'
+            excludes 'xalan' // IVY-1006 - use xalan 2.7.0 to avoid (see below)
+            excludes 'xml-apis' // GROOVY-3356
+            exported = false
+        }
     }
 }
