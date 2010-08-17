b352c0f2f560c9092f24b7571e2b8024965252a7
Luke Daley <ld@ldaley.com>
2010-08-17 22:03:24 +1000

    Updated authorship.

--- JmsGrailsPlugin.groovy
+++ JmsGrailsPlugin.groovy
@@ -15,9 +15,9 @@ class JmsGrailsPlugin {
     static LOG = LogFactory.getLog('grails.plugin.jms.JmsGrailsPlugin')
     
     def version = "0.5.2"
-    def author = "Luke Daley"
-    def authorEmail = "ld@ldaley.com"
-    def title = "This plugin adds MDB functionality to services."
+    def author = "Grails Plugin Collective"
+    def authorEmail = "grails.plugin.collective@gmail.com"
+    def title = "JMS integration for Grails"
     def grailsVersion = "1.2.0 > *"
     
     def loadAfter = ['services', 'controllers']
