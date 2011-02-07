23690151ff5dd266b2f88bbe2ea9df5b59279a18
Bernardo Gomez Palacio <bernardo.gomezpalacio@gmail.com>
2011-02-07 00:05:29 -0800

    Creating a broker used for testing that stores peristed data in target/activemq-data

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,19 +1,27 @@
 import org.apache.activemq.ActiveMQConnectionFactory
 import org.springframework.jms.connection.SingleConnectionFactory
 
-/**
- * @todo Establish kahaDB's directory (e.g. directory="activemq-data") so that it lives in target.
- */
 beans = {
-    jmsConnectionFactory(SingleConnectionFactory) {
-        targetConnectionFactory = { ActiveMQConnectionFactory cf ->
-            brokerURL = 'vm://localhost'
+    xmlns amq:"http://activemq.apache.org/schema/core"
+
+    amq.broker( xmlns:"http://activemq.apache.org/schema/core",
+                brokerName:"localhost",
+                dataDirectory:"target/activemq-data" ){
+
+        amq.transportConnectors{
+             amq.transportConnector(name:"vm", uri:"vm://localhost" )
         }
     }
-    
+
+    jmsConnectionFactory(SingleConnectionFactory) {
+        targetConnectionFactory = { ActiveMQConnectionFactory cf ->
+            brokerURL = 'vm://localhost?create=false&waitForStart=100'
+        }
+    }
+
     otherJmsConnectionFactory(SingleConnectionFactory) {
         targetConnectionFactory = { ActiveMQConnectionFactory cf ->
-            brokerURL = 'vm://localhost'
+            brokerURL = 'vm://localhost?create=false&waitForStart=100'
         }
     }
 }
