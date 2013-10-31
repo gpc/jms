3a5a167fa793c9ed5e7124122d45b550e3ae1dd8
Burt Beckwith <burt@burtbeckwith.com>
2013-10-31 14:36:45 -0400

    updated to 2.x for development

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,49 +1,22 @@
 /*
-* Copyright 2010 Grails Plugin Collective
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2010 Grails Plugin Collective
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 log4j = {
     error ''
     debug 'grails.plugin.jms'
     debug 'grails.app.service'
-}
-
-dataSource {
-    pooled = true
-    driverClassName = "org.hsqldb.jdbcDriver"
-    username = "sa"
-    password = ""
-}
-hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = true
-    cache.provider_class = 'com.opensymphony.oscache.hibernate.OSCacheProvider'
-}
-
-environments {
-    development {
-        dataSource {
-            dbCreate = "create-drop"
-            url = "jdbc:hsqldb:mem:devDB"
-        }
-    }
-    test {
-        dataSource {
-            dbCreate = "create-drop"
-            url = "jdbc:hsqldb:mem:testDb"
-        }
-    }
 }
 
 beans {
