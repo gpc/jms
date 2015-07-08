51ea1e43986e288792f6e486065769b6a63d8b9d
Jeff Scott Brown <jeff@jeffandbetsy.net>
2015-07-08 16:13:59 -0500

    replace some references to master with references to grails3 for now

--- travis-build.sh
+++ travis-build.sh
@@ -8,7 +8,7 @@ filename=$(basename "$filename")
 
 EXIT_STATUS=0
 echo "Publishing archives for branch $TRAVIS_BRANCH"
-if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'grails3' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
   echo "Publishing archives"
 
@@ -28,8 +28,8 @@ if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST
   git clone https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git -b gh-pages gh-pages --single-branch > /dev/null
   cd gh-pages
 
-  # If this is the master branch then update the snapshot
-  if [[ $TRAVIS_BRANCH == 'master' ]]; then
+  # If this is the grails3 branch then update the snapshot
+  if [[ $TRAVIS_BRANCH == 'grails3' ]]; then
     mkdir -p snapshot
     cp -r ../build/docs/manual/. ./snapshot/
 
